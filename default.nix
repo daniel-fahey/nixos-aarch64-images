38f0a3c393dd7d6ea159212588af2a8b9a9f15bc
Daniel Fahey <daniel.fahey@pm.me>
2024-09-12 19:43:16 +0100

    WIP: u-boot appears to work but hung at `Starting kernel ...`

--- default.nix
+++ default.nix
@@ -13,6 +13,34 @@ let
     inherit uboot;
     inherit aarch64Image buildImage;
   };
+
+  ubootHelios64 = aarch64Pkgs.buildUBoot rec {
+    version = "2022.07";
+    src = pkgs.fetchurl {
+      url = "https://ftp.denx.de/pub/u-boot/u-boot-${version}.tar.bz2";
+      hash = "sha256-krCOtJwk2hTBrb9wpxro83zFPutCMOhZrYtnM9E9z14=";
+    };
+    patches = [
+      ./0001-rk3399-update-memory-layout-for-larger-modern-kernels.patch
+    ] ++ extraPatches;
+    extraPatches = [
+      (pkgs.fetchpatch {
+        url = "https://raw.githubusercontent.com/armbian/build/main/patch/u-boot/u-boot-rockchip64/add-board-helios64.patch";
+        hash = "sha256-REwp1F7ni/PXXMzuLxjO3PonCuyAxqIjVaqABTORuHY=";
+      })
+    ];
+    postPatch = ''
+      cat include/configs/rk3399_common.h
+      patchShebangs tools
+      patchShebangs scripts
+      patchShebangs arch/arm/mach-rockchip
+    '';
+    defconfig = "helios64-rk3399_defconfig";
+    extraMeta.platforms = ["aarch64-linux"];
+    BL31 = "${aarch64Pkgs.armTrustedFirmwareRK3399}/bl31.elf";
+    filesToInstall = [ "u-boot.itb" "idbloader.img" ".config" ];
+  };
+
 in {
   inherit aarch64Image;
 
@@ -21,4 +49,5 @@ in {
   roc-pc-rk3399 = rockchip aarch64Pkgs.ubootROCPCRK3399;
   pinebookPro = rockchip aarch64Pkgs.ubootPinebookPro;
   rock4cPlus = rockchip aarch64Pkgs.ubootRock4CPlus;
+  helios64 = rockchip ubootHelios64;
 }
--- pkgs/aarch64-image/default.nix
+++ pkgs/aarch64-image/default.nix
@@ -4,9 +4,9 @@ let
     # unfortunally there is no easy way right now to reproduce the same evaluation
     # as hydra, since `pkgs.path` is embedded in the binary
     # To get a new url use:
-    # $ curl -s -L -I -o /dev/null -w '%{url_effective}' "https://hydra.nixos.org/job/nixos/release-20.03/nixos.sd_image.aarch64-linux/latest/download/1"
-    url = "https://hydra.nixos.org/build/269682674/download/1/nixos-sd-image-24.05.3914.c3d4ac725177-aarch64-linux.img.zst";
-    sha256 = "0g11hpsl8sw44vi7f6pmhxi1x59v61mj2vcpv5cz3j98r58w13y6";
+    # $ curl -s -L -I -o /dev/null -w '%{url_effective}' "https://hydra.nixos.org/job/nixos/trunk-combined/nixos.sd_image.aarch64-linux/latest/download/1"
+    url = "https://hydra.nixos.org/build/272559770/download/1/nixos-sd-image-24.11pre680062.4f807e894028-aarch64-linux.img.zst";
+    hash = "sha256-ThCT9wSq94VrPSyNqYU2oDMR/LIJzXfj1nLy0W7TMQE=";
   }).overrideAttrs (final: prev: {
     __structuredAttrs = true;
     unsafeDiscardReferences.out = true;
