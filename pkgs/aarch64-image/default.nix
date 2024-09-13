145ad58feb9a6428daced5f529704bf3289f09d8
Daniel Fahey <daniel.fahey@pm.me>
2024-09-13 10:26:51 +0100

    use latest kernel

--- pkgs/aarch64-image/default.nix
+++ pkgs/aarch64-image/default.nix
@@ -4,9 +4,9 @@ let
     # unfortunally there is no easy way right now to reproduce the same evaluation
     # as hydra, since `pkgs.path` is embedded in the binary
     # To get a new url use:
-    # $ curl -s -L -I -o /dev/null -w '%{url_effective}' "https://hydra.nixos.org/job/nixos/trunk-combined/nixos.sd_image.aarch64-linux/latest/download/1"
-    url = "https://hydra.nixos.org/build/272559770/download/1/nixos-sd-image-24.11pre680062.4f807e894028-aarch64-linux.img.zst";
-    hash = "sha256-ThCT9wSq94VrPSyNqYU2oDMR/LIJzXfj1nLy0W7TMQE=";
+    # $ curl -s -L -I -o /dev/null -w '%{url_effective}' "https://hydra.nixos.org/job/nixos/trunk-combined/nixos.sd_image_new_kernel_no_zfs.aarch64-linux/latest/download/1"
+    url = "https://hydra.nixos.org/build/272559336/download/1/nixos-sd-image-24.11pre680062.4f807e894028-aarch64-linux.img.zst";
+    hash = "sha256-CxyYAaj9cugnDC6cwww1k0gIWNjftapGO+Q6Gs6cA5A=";
   }).overrideAttrs (final: prev: {
     __structuredAttrs = true;
     unsafeDiscardReferences.out = true;
